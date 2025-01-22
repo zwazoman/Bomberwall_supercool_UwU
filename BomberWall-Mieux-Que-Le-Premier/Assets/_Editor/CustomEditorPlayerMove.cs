c3d0f6ebdedebc956d19fb1f0904ca6e135c5bcf
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-01-22 10:20:13 +0100

    sdbn

--- BomberWall-Mieux-Que-Le-Premier/Assets/_Editor/CustomEditorPlayerMove.cs
+++ BomberWall-Mieux-Que-Le-Premier/Assets/_Editor/CustomEditorPlayerMove.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 [CustomEditor(typeof(PlayerMove))]
 public class CustomEditorPlayerMove : Editor
 {
+    private MeshRenderer m_Renderer;
+
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
+        DrawLine();
 
-        if (GUILayout.Button("Explode Bomb"))
+        if (GUILayout.Button("Surprise"))
         {
+            PlayerMove Target = (PlayerMove)target;
+            Target.TryGetComponent<MeshRenderer>(out m_Renderer);
+            m_Renderer.sharedMaterial.color = Random.ColorHSV();
         }
+        DrawLine();
+    }
+    void DrawLine(int i_height = 1)
+    {
+        Rect rect = EditorGUILayout.GetControlRect(false, i_height);
+        rect.height = i_height;
+        EditorGUI.DrawRect(rect, new Color(0.5f, 0.5f, 0.5f, 1));
     }
 }
